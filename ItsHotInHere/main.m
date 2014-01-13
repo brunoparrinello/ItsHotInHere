89ce959dc9529cb7ecb00b7938086445952419a8
brunoparrinello <bparrine@yahoo-inc.com>
2014-01-12 16:53:49 -0800

    Initial Commit

---
+++ ItsHotInHere/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ItsHotInHere
+//
+//  Created by Bruno Parrinello on 1/12/14.
+//  Copyright (c) 2014 Bruno Parrinello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ItsHotInHereTests/ItsHotInHereTests.m
@@ -0,0 +1,34 @@
+//
+//  ItsHotInHereTests.m
+//  ItsHotInHereTests
+//
+//  Created by Bruno Parrinello on 1/12/14.
+//  Copyright (c) 2014 Bruno Parrinello. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ItsHotInHereTests : XCTestCase
+
+@end
+
+@implementation ItsHotInHereTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
